ff516f2b8d342ab5eb643e619a59a09b5cf6621e
OneZale1995 <442342634@qq.com>
2019-08-18 20:26:35 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['excpect' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -16,11 +22,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', ['user' => $user]);
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if($request->avatar){
