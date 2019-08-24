5cf93be785b8ce332f41948308e4372523264ea6
OneZale1995 <442342634@qq.com>
2019-08-24 22:56:01 +0800

    消息通知有新回复

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$10$Mw/2R21UymtS8hibzK0QXuDWN4OeQsB6QhiYhDGVrM6Do3iDzhZNq', // password
         'remember_token' => Str::random(10),
         'introduction' => $faker->sentence(),
         'created_at' => $data_time,
