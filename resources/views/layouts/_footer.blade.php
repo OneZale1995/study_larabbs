220e0e5122eb6edafcc5346ff84e3a920e35f66a
OneZale1995 <442342634@qq.com>
2019-08-26 21:57:03 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="https://github.com/OneZale1995" target="_blank">OneZale</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:442342634@qq.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
