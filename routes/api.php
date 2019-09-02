238f2071b2f683411e52ba2e65db02cf8e356c9b
OneZale1995 <442342634@qq.com>
2019-09-02 22:23:19 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,23 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+], function ($api) {
+    $api->group([
+        'middleware' => 'api.throttle', // 调用频率限制
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
 
-$api->version('v2', function($api) {
-    $api->get('version', function() {
+$api->version('v2', function ($api) {
+    $api->get('version', function () {
         return response('this is version v2');
     });
 });
