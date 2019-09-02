602ca45d7a39affa390f88a9397e9e018d5c0046
OneZale1995 <442342634@qq.com>
2019-09-02 23:40:20 +0800

    add socialiteproviders

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
 
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,12 +15,16 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
         ],
-        \Illuminate\Auth\Events\Verified::class => [
-            \App\Listeners\EmailVerified::class,
-        ],
+//        Registered::class => [
+//            SendEmailVerificationNotification::class,
+//        ],
+//        \Illuminate\Auth\Events\Verified::class => [
+//            \App\Listeners\EmailVerified::class,
+//        ],
     ];
 
     /**
@@ -35,3 +39,5 @@ class EventServiceProvider extends ServiceProvider
         //
     }
 }
+
+
